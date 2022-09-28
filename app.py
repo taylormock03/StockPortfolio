f9bda035212b93a1c599819b6f3cee14a4328104
Taylor Mock <taylormock03@gmail.com>
2022-09-28 13:28:40 +1000

    Established basic displaying of portfolios on home page

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from Lib.portfolio import Portfolio
 import json
 
 app = Flask(__name__)
 
+@app.route('/test')
+def index():
+    a = Portfolio("test", 123)
+    portfolios = [a]
+    return render_template('index.html', portfolios=portfolios)
+
 if __name__ == "__main__":
+    
     app.run(port=12345, debug=True)
